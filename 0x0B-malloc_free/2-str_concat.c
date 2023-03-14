7c719d24ae4c9ff3b344b73bd9d52967b044ed83
bmulinda <bfemail@gmail.com>
2023-03-14 14:02:37 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -31,8 +31,8 @@ char *str_concat(char *s1, char *s2)
 	if (conct == NULL)
 		return (NULL);
 	i = ci = 0;
-	while (s1[i] != '\0\)
-			{
+	while (s1[i] != '\0')
+	{
 			conct[i] = s1[i];
 			i++;
 			}
@@ -43,4 +43,4 @@ char *str_concat(char *s1, char *s2)
 			}
 			conct[i] = '\0';
 			return (conct);
-			}
+}
